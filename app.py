8d9f69dd6bff6174f1e3b7b5fda856def17ed249
claudio-muroni <muroni.claudio@gmail.com>
2025-08-30 18:45:47 +0200

    First call to Supabase DB

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpbHhzbXZnYWhjZWVndmh6ZGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4NDQyMzMsImV4cCI6MjA1MzQyMDIzM30.pfSVEKiUtrCDzfH844CER82ce13stzGjTVtCeCwaMhA"
+
 @app.route('/')
 def index():
+
+    response = requests.get("https://filxsmvgahceegvhzdee.supabase.co/rest/v1/presidenti?apikey="+ANON_KEY)
+    if response.status_code == 200:
+            presidents = response.json()
+            names = [p["nome"] for p in presidents]
+            
+            return names
+
+
     return "IFESIF"
