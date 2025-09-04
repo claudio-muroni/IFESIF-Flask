85bb72b11a54f16e3e44448b4fe4c18cbfdf8502
claudio-muroni <muroni.claudio@gmail.com>
2025-09-05 00:02:17 +0200

    Started working on president page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -10,11 +10,19 @@ def index():
     response = requests.get("https://filxsmvgahceegvhzdee.supabase.co/rest/v1/presidenti?apikey="+ANON_KEY)
     if response.status_code == 200:
             result = response.json()
-            presidents = [p["cognome"] for p in result]
+            presidents = [p["cognome"] for p in result if p["attivo"] == True]
             return render_template("index.html", presidents=presidents)
 
     return "IFESIF"
 
 @app.route('/pres')
 def pres():
+    pres = request.args.get("name")
+
+    response = requests.get("https://filxsmvgahceegvhzdee.supabase.co/rest/v1/contratti?apikey="+ANON_KEY)
+    if response.status_code == 200:
+        result = response.json()
+        contracts = [c for c in result if c["cognome_presidente"] == pres]
+        return render_template("president.html", contracts=contracts)
+
     return "Presidente"
