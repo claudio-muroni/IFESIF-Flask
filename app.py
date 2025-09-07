aaaa7db23dcbbd6229ed9c5e8ec28a89e5a953d7
claudio-muroni <muroni.claudio@gmail.com>
2025-09-07 16:05:49 +0200

    added setting file

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request
 import requests
+import settings
 
 app = Flask(__name__)
 
-ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpbHhzbXZnYWhjZWVndmh6ZGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4NDQyMzMsImV4cCI6MjA1MzQyMDIzM30.pfSVEKiUtrCDzfH844CER82ce13stzGjTVtCeCwaMhA"
-
 @app.route('/')
 def index():
-    response = requests.get("https://filxsmvgahceegvhzdee.supabase.co/rest/v1/presidenti?apikey="+ANON_KEY)
+    response = requests.get(settings.DB_URL+"/rest/v1/presidenti?apikey="+settings.DB_KEY)
     if response.status_code == 200:
             result = response.json()
             presidents = [p["cognome"] for p in result if p["attivo"] == True]
@@ -19,7 +18,7 @@ def index():
 def pres():
     pres = request.args.get("name")
 
-    response = requests.get("https://filxsmvgahceegvhzdee.supabase.co/rest/v1/contratti?apikey="+ANON_KEY)
+    response = requests.get(settings.DB_URL+"/rest/v1/contratti?apikey="+settings.DB_KEY)
     if response.status_code == 200:
         result = response.json()
         contracts = [c for c in result if c["cognome_presidente"] == pres]
---
+++ settings.py
@@ -0,0 +1,4 @@
+import os
+
+DB_URL: str = os.environ.get("ifesif_supabase_url")
+DB_KEY: str = os.environ.get("ifesif_supabase_key")
