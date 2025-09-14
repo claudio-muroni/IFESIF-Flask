73f15bd3d6167254f03a5645f3814a22ba20e3c9
claudio-muroni <muroni.claudio@gmail.com>
2025-09-14 22:28:27 +0200

    app.run() and gunicorn added

--- app.py
+++ app.py
@@ -34,3 +34,6 @@ def pres():
         return render_template("president.html", contracts=contracts, pres=pres, budget=budget)
 
     return "Presidente non disponibile"
+
+if __name__ == "__main__":
+    app.run()
