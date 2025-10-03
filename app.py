189c953e1ef8105d38af7e40288e2a3e7cb67969
claudio-muroni <muroni.claudio@gmail.com>
2025-10-03 22:30:23 +0200

    using both name and surname to navigate

--- app.py
+++ app.py
@@ -11,27 +11,28 @@ def index():
     response = requests.get(settings.DB_URL+"/rest/v1/presidenti?apikey="+settings.DB_KEY)
     if response.status_code == 200:
             result = response.json()
-            presidents = [p["cognome"] for p in result if p["attivo"] == True]
+            presidents = [(p["cognome"],p["nome"]) for p in result if p["attivo"] == True]
             return render_template("index.html", presidents=presidents)
 
     return "IFESIF"
 
 @app.route('/pres')
 def pres():
-    pres = request.args.get("name")
+    surname = request.args.get("surname")
+    name = request.args.get("name")
 
     response = requests.get(settings.DB_URL+"/rest/v1/contratti?apikey="+settings.DB_KEY)
     response2 = requests.get(settings.DB_URL+"/rest/v1/presidenti?apikey="+settings.DB_KEY)
     if response.status_code == 200 and response2.status_code == 200:
         result = response.json()
-        contracts = [c for c in result if c["cognome_presidente"] == pres]
+        contracts = [c for c in result if c["cognome_presidente"] == surname and c["nome_presidente"] == name]
         contracts = sorted(contracts, key=itemgetter('ruolo'), reverse=True)
 
         result2 = response2.json()
-        budget = [p for p in result2 if p["cognome"] == pres]
+        budget = [p for p in result2 if p["cognome"] == surname]
         budget = budget[0]["cash"]
 
-        return render_template("president.html", contracts=contracts, pres=pres, budget=budget)
+        return render_template("president.html", contracts=contracts, surname=surname, name=name, budget=budget)
 
     return "Presidente non disponibile"
 
